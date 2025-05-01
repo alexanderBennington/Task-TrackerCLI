a1637d29f21c42f38289ce49011890f0c5e96397
alexanderBennington <kevalexan15@outlook.com>
2025-05-01 15:38:44 -0600

    Colocación de colores

--- src/main/java/TaskTrackerApp.java
+++ src/main/java/TaskTrackerApp.java
@@ -1,4 +1,5 @@
 import controller.TaskController;
+import utils.AnsiColors;
 
 import java.nio.file.*;
 import java.util.Scanner;
@@ -17,20 +18,15 @@ public class TaskTrackerApp {
                 System.out.println("📂 File exists: " + FILE_NAME);
             }
         } catch (Exception e) {
-            System.out.println("❌ Failed to create the JSON file: " + e.getMessage());
+            System.out.println(AnsiColors.RED + "❌ Failed to create the JSON file: " + e.getMessage() + AnsiColors.RESET);
         }
     }
 
     private static void showWelcome(Scanner console) {
         TaskController taskC = new TaskController();
         boolean exit = false;
-        String RESET = "\u001B[0m";
-        String BLUE = "\u001B[34m";
-        String GREEN = "\u001B[32m";
-        String CYAN = "\u001B[36m";
-        String YELLOW = "\u001B[33m";
 
-        System.out.println(BLUE + """
+        System.out.println(AnsiColors.BLUE + """
                 
                 
                 ___________              __     ___________                     __                \s
@@ -41,12 +37,12 @@ public class TaskTrackerApp {
                                \\/     \\/     \\/                       \\/     \\/     \\/    \\/      \s
                 
                 
-                """ + RESET);
+                """ + AnsiColors.RESET);
 
-        System.out.println(CYAN + "🔧 Welcome to Task Tracker CLI" + RESET);
-        System.out.println(GREEN + "📅 Organize your tasks easily from Command Line Interface." + RESET);
+        System.out.println(AnsiColors.CYAN + "🔧 Welcome to Task Tracker CLI" + AnsiColors.RESET);
+        System.out.println(AnsiColors.GREEN + "📅 Organize your tasks easily from Command Line Interface." + AnsiColors.RESET);
         do {
-            System.out.println(YELLOW + """
+            System.out.println(AnsiColors.YELLOW + """
                 ╔═══════════════════════════════╗
                 ║  1. View all tasks            ║
                 ║  2. Create a new task         ║
@@ -54,15 +50,18 @@ public class TaskTrackerApp {
                 ║  4. Delete task               ║
                 ║  5. Search for a task by ID   ║
                 ║  6. Update status             ║
-                ║  7. Exit                      ║
+                ║  7. View pending tasks        ║
+                ║  8. View tasks in progress    ║
+                ║  9. View completed tasks      ║
+                ║  10. Exit                     ║
                 ╚═══════════════════════════════╝
-                """ + RESET);
+                """ + AnsiColors.RESET);
             try {
                 System.out.print("Select an option: ");
                 int option = Integer.parseInt(console.nextLine());
                 exit = taskC.selectedOption(console, option);
             } catch (NumberFormatException e) {
-                System.out.println("❌ Invalid input. Please enter a number between 1 and 7.");
+                System.out.println(AnsiColors.RED + "❌ Invalid input. Please enter a number between 1 and 10." + AnsiColors.RESET);
             }
         } while(!exit);
     }
