8e3e34598870a5735386d5b0ced072dcdf9382ec
alexanderBennington <kevalexan15@outlook.com>
2025-05-01 15:38:17 -0600

    Colocación de colores

--- src/main/java/controller/TaskController.java
+++ src/main/java/controller/TaskController.java
@@ -4,6 +4,7 @@ import DAO.ITaskDAO;
 import DAO.TaskDAO;
 import model.Status;
 import model.Task;
+import utils.AnsiColors;
 
 import java.util.List;
 import java.util.Scanner;
@@ -20,11 +21,11 @@ public class TaskController {
                 Task task = saveTaskOption(console);
                 if (taskDAO.saveTask(task)) {
                     System.out.println("Updating JSON...");
-                    System.out.println("✅ Task saved successfully!");
+                    System.out.println(AnsiColors.BLUE + "✅ Task saved successfully!" + AnsiColors.RESET);
                     tasks = taskDAO.getAllTasks();
                     viewTasks(tasks);
                 } else {
-                    System.out.println("❌ Task not saved.");
+                    System.out.println(AnsiColors.RED + "❌ Task not saved." + AnsiColors.RESET);
                 }
             }
             case 3 -> {
@@ -32,76 +33,83 @@ public class TaskController {
                 Task task = updateTaskOption(console);
                 if (taskDAO.updateTask(task)) {
                     System.out.println("Updating JSON...");
-                    System.out.println("✅ Task updated successfully!");
+                    System.out.println(AnsiColors.BLUE + "✅ Task updated successfully!" + AnsiColors.RESET);
                     tasks = taskDAO.getAllTasks();
                     viewTasks(tasks);
                 } else {
-                    System.out.println("❌ Task not updated.");
+                    System.out.println(AnsiColors.RED + "❌ Task not updated." + AnsiColors.RESET);
                 }
             }
             case 4 -> {
-                int idTask = 0;
-                try {
-                    viewTasks(tasks);
-                    System.out.println("Enter task ID: ");
-                    idTask = Integer.parseInt(console.nextLine());
-                } catch (NumberFormatException e) {
-                    System.out.println("❌ Invalid input. Please enter a number.");
-                }
+                viewTasks(tasks);
+                int idTask = enterIdTask(console);
 
                 if(taskDAO.deleteTaskById(idTask)){
                     System.out.println("Updating JSON...");
-                    System.out.println("✅ Task deleted successfully!");
+                    System.out.println(AnsiColors.BLUE + "✅ Task deleted successfully!" + AnsiColors.RESET);
                     tasks = taskDAO.getAllTasks();
                     viewTasks(tasks);
                 } else {
-                    System.out.println("❌ Task not deleted.");
+                    System.out.println(AnsiColors.RED + "❌ Task not deleted." + AnsiColors.RESET);
                 }
             }
             case 5 -> {
-                int idTask = 0;
-                Task task = null;
-                try {
-                    System.out.println("Enter task ID: ");
-                    idTask = Integer.parseInt(console.nextLine());
-                    task = taskDAO.getTaskById(idTask);
-                } catch (NumberFormatException e) {
-                    System.out.println("❌ Invalid input. Please enter a number.");
-                }
+                int idTask = enterIdTask(console);
+                Task task = taskDAO.getTaskById(idTask);
 
                 if(task == null){
-                    System.out.println("⚠️ Task not found");
+                    System.out.println(AnsiColors.RED + "⚠️ Task not found" + AnsiColors.RESET);
                     viewTasks(tasks);
                 } else {
                     System.out.println(task);
                 }
             }
             case 6 -> {
-                int idTask = 0;
-                Status status = null;
-                try {
-                    viewTasks(tasks);
-                    System.out.println("Enter task ID: ");
-                    idTask = Integer.parseInt(console.nextLine());
-                    status = selectedStatus(console);
-                } catch (NumberFormatException e) {
-                    System.out.println("❌ Invalid input. Please enter a number.");
-                }
+                viewTasks(tasks);
+                int idTask = enterIdTask(console);
+                Status status = selectedStatus(console);
 
                 if(taskDAO.updateStatusById(idTask, status)){
                     System.out.println("Updating JSON...");
-                    System.out.println("✅ Status updated successfully!");
+                    System.out.println(AnsiColors.BLUE + "✅ Status updated successfully!" + AnsiColors.RESET);
                     tasks = taskDAO.getAllTasks();
                     viewTasks(tasks);
                 } else {
-                    System.out.println("❌ Status not updated.");
+                    System.out.println(AnsiColors.RED + "❌ Status not updated." + AnsiColors.RESET);
                 }
             }
             case 7 -> {
+                List<Task> todoTasks = taskDAO.getTodoTasks();
+                if (todoTasks.isEmpty()) {
+                    System.out.println(AnsiColors.RED + "📭 No tasks found." + AnsiColors.RESET);
+                } else {
+                    todoTasks.forEach(System.out::println);
+                }
+                System.out.println();
+            }
+            case 8 -> {
+                List<Task> progressTasks = taskDAO.getProgressTasks();
+                if (progressTasks.isEmpty()) {
+                    System.out.println(AnsiColors.RED + "📭 No tasks found." + AnsiColors.RESET);
+                } else {
+                    progressTasks.forEach(System.out::println);
+                }
+                System.out.println();
+            }
+            case 9 -> {
+                List<Task> doneTasks = taskDAO.getDoneTasks();
+                if (doneTasks.isEmpty()) {
+                    System.out.println(AnsiColors.RED + "📭 No tasks found." + AnsiColors.RESET);
+                } else {
+                    doneTasks.forEach(System.out::println);
+                }
+                System.out.println();
+            }
+            case 10 -> {
                 System.out.println("👋 Goodbye!");
                 return true;
             }
-            default -> System.out.println("⚠️ Please enter a number between 1 and 7.");
+            default -> System.out.println(AnsiColors.RED + "⚠️ Please enter a number between 1 and 10." + AnsiColors.RESET);
         }
         return false;
     }
@@ -124,12 +132,26 @@ public class TaskController {
     }
 
     private static Task updateTaskOption(Scanner console){
-        System.out.println("ID of the task to be modified: ");
-        int idtask = Integer.parseInt(console.nextLine());
+        int idTask = enterIdTask(console);
         System.out.println("New Description: ");
         String description = console.nextLine();
         Status status = selectedStatus(console);
-        return new Task(idtask, description, status);
+        return new Task(idTask, description, status);
+    }
+
+    private static int enterIdTask(Scanner console){
+        int idtask = 0;
+        boolean isNumber = false;
+        while(!isNumber){
+            try{
+                System.out.println("Enter ID: ");
+                idtask = Integer.parseInt(console.nextLine());
+                isNumber = true;
+            }catch (NumberFormatException e) {
+                System.out.println(AnsiColors.RED + "❌ Invalid input. Please enter a number" + AnsiColors.RESET);
+            }
+        }
+        return idtask;
     }
 
     private static Status selectedStatus(Scanner console){
@@ -138,12 +160,12 @@ public class TaskController {
         boolean validStatus = false;
         while (!validStatus){
             try {
-                System.out.println("""
+                System.out.println(AnsiColors.CYAN + """
                             Status
                             1. TODO
                             2. IN_PROGRESS
                             3. DONE
-                            """);
+                            """ + AnsiColors.RESET);
                 System.out.print("Select: ");
                 progress = Integer.parseInt(console.nextLine());
                 switch (progress){
@@ -159,10 +181,10 @@ public class TaskController {
                         status = Status.DONE;
                         validStatus = true;
                     }
-                    default -> System.out.println("⚠️ Please enter a number between 1 and 3.");
+                    default -> System.out.println(AnsiColors.RED + "⚠️ Please enter a number between 1 and 3." + AnsiColors.RESET);
                 }
             } catch (NumberFormatException e) {
-                System.out.println("❌ Invalid input. Please enter a number between 1 and 3.");
+                System.out.println(AnsiColors.RED + "❌ Invalid input. Please enter a number between 1 and 3." + AnsiColors.RESET);
             }
         }
         return status;
